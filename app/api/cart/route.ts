543a67c5b0b59f102ed90a50548d524693ed2420
AmanPatre <amanpatre25@gmail.com>
2026-03-06 23:08:41 +0530

    Updated

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
-const cartdata = {
+export const cartdata = {
 
     cartItems: [
         {
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,16 +1,7 @@
 import { CartData } from '../context/CartContext';
 import CartPageClient from './CartPageClient';
+import { cartdata } from '../api/cart/route';
 
-async function getCartData(): Promise<CartData> {
-    const URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3001";
-    const res = await fetch(`${URL}/api/cart`, { cache: "no-store" });
-    if (!res.ok) throw new Error('Failed to fetch cart data');
-    return res.json();
-
-
-
-}
-export default async function () {
-    const cartData = await getCartData();
-    return <CartPageClient initial={cartData} />
+export default async function Page() {
+    return <CartPageClient initial={cartdata as CartData} />
 }
